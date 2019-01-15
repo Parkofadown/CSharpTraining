227a5ea614af8684e0dadd2d631f746101f69cc2
richa <richard.boyd1218@gmail.com>
2019-01-14 20:11:23 -0500

    Add project files.

---
+++ CSharpTraining/main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var number = "1234";
+                byte i = Convert.ToByte(number);
+                Console.WriteLine(i);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could not be converted to a byte.");
+            }
+           
+        }
+    }
+}
